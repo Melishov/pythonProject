3591849f774b4ffe0db2d92b93f124eb5169b42a
Ilya Melishov <melishov@mail.ru>
2023-12-10 00:15:49 +0500

    Новинки

--- List/lottery.py
+++ List/lottery.py
@@ -6,8 +6,6 @@ def main():
     for number in range(NUMBERS):
         numbers.append(number+1)
     num_list=[]
-    #for count in range(COUNT_OF_NUM):
-        #num_list.append(random.randint(0,9))
     num_list=random.sample(numbers,k=COUNT_OF_NUM)
     for element in num_list:
         print('|',element,'|',end="")
