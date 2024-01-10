fc1d25caadf31a6410068ba4c94be1f62a201a62
Melishov <melishov@gmail.com>
2024-01-10 20:24:13 +0500

    Новые проги

---
+++ CH/example_1.py
@@ -0,0 +1,6 @@
+mystring='ПривеТ, ЛюДиШки!'
+upper=0
+for ch in mystring:
+    if ch.isupper():
+        upper+=1
+print(upper)
---
+++ CH/example_ch_2.py
@@ -0,0 +1,6 @@
+def main():
+    days_string='Понедельник Вторник Среда'
+    days_list=days_string.split(' ')
+    print(days_list)
+if __name__=="__main__":
+    main()
